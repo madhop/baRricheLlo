f7c185cc428db00389078f57b33219723fb5b086
Umberto <umbertofazio92@gmail.com>
2019-10-19 11:28:42 +0200

    Curvature

--- compute_state_features.py
+++ compute_state_features.py
@@ -18,9 +18,9 @@ def position(r, r1, p): # inputs must be np.array
 
     print("rotation_angle: ", rotation_angle)
     rotation_matrix = np.array([[np.cos(rotation_angle), -np.sin(rotation_angle)],[np.sin(rotation_angle), np.cos(rotation_angle)]])
-    print("rotation_matrix: \n", rotation_matrix)
+    #print("rotation_matrix: \n", rotation_matrix)
     rel_p = np.dot(rotation_matrix,p-r)#np.array([(p-r)[0]*np.cos(rotation_angle)-(p-r)[1]*np.sin(rotation_angle), (p-r)[0]*np.sin(rotation_angle)+(p-r)[1]*np.cos(rotation_angle)])
-    print("rel_p: ", rel_p)
+    #print("rel_p: ", rel_p)
     #print("dist: ", distance(r,r1,p))
     rho = np.linalg.norm(p-r)
     if np.array_equal(p-r, np.array([0,0])):
@@ -29,22 +29,38 @@ def position(r, r1, p): # inputs must be np.array
         theta = -np.arccos(np.dot(np.array([1,0]),rel_p)/(1 * np.linalg.norm(rel_p)))
     else:
         theta = np.arccos(np.dot(np.array([1,0]),rel_p)/(1 * np.linalg.norm(rel_p)))
-    print('theta: ', theta)
     return rel_p, rho, theta
 
-"""# test state features
+def curvature(p, p1, p2):   # pt, pt-1 and pt-2
+    """ return the curvature.
+        Negative if p2 if on the right of p
+    """
+    if p[1] > p1[1]:
+        rotation_angle = -np.arccos(np.dot(np.array([1,0]),p-p1)/(1 * np.linalg.norm(p-p1)))
+    else:
+        rotation_angle = np.arccos(np.dot(np.array([1,0]),p-p1)/(1 * np.linalg.norm(p-p1)))
+    rotation_matrix = np.array([[np.cos(rotation_angle), -np.sin(rotation_angle)],[np.sin(rotation_angle), np.cos(rotation_angle)]])
+    rel_p2 = np.dot(rotation_matrix,p2-p1)
+    c = np.arccos(np.dot(p-p1,p2-p1)/(np.linalg.norm(p-p1) * np.linalg.norm(p2-p1)))
+    if rel_p2[1] < 0:
+        c = -c
+    print("curvature: ", c)
+    return c
+
+# test state features
 r = np.array([1,1])
-r1 = np.array([2,2])
-p = np.array([1,0])"""
+r1 = np.array([0,1])
+p = np.array([1,2])
 
-r = np.array([ref_df.iloc[0]['x'], ref_df.iloc[0]['y']])
+"""r = np.array([ref_df.iloc[0]['x'], ref_df.iloc[0]['y']])
 r1 = np.array([ref_df.iloc[10]['x'], ref_df.iloc[10]['y']])
-p = np.array([car_df.iloc[0]['x'], car_df.iloc[0]['y']])
+p = np.array([car_df.iloc[0]['x'], car_df.iloc[0]['y']])"""
 rel_p, rho, theta = position(r, r1, p)
+cur = curvature(r1, r, p)
 
-
-plt.arrow(r[0],r[1], (r1-r)[0], (r1-r)[1], head_width=0.07, head_length=0.5, fc='k', ec='k')
-plt.plot([r[0], r1[0], p[0]],[r[1], r1[1], p[1]], '*')
+plt.arrow(r[0],r[1], (r1-r)[0], (r1-r)[1], head_width=0.01, head_length=0.1, fc='r', ec='r')
+plt.arrow(r[0],r[1], (p-r)[0], (p-r)[1], head_width=0.01, head_length=0.1, fc='g', ec='g')
+plt.plot([r[0], r1[0], p[0]],[r[1], r1[1], p[1]], 'o')
 plt.show()
 
 """for i in range(2):
