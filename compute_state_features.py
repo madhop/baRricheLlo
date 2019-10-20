70a891922b1a3d56dbfc6f0115ece4d3073f2477
Umberto <umbertofazio92@gmail.com>
2019-10-20 21:29:11 +0200

    speed and acceleration

--- compute_state_features.py
+++ compute_state_features.py
@@ -9,19 +9,24 @@ car_df = pd.read_csv('car_trajectory_monza.csv') # car trajectory
 def distance(r,r1,p):   # distance line-point
     return np.linalg.norm((r1[1]-r[1])*p[0]-(r1[0]-r[0])*p[1]+r1[0]*r[1]-r1[1]*r[0])/np.linalg.norm(r1-r)
 
+def realtive_features(p, r):
+    if r[1] > 0:
+        rotation_angle = -np.arccos(np.dot(np.array([1,0]),r)/(1 * np.linalg.norm(r)))
+    else:
+        rotation_angle = np.arccos(np.dot(np.array([1,0]),r)/(1 * np.linalg.norm(r)))
+    rotation_matrix = np.array([[np.cos(rotation_angle), -np.sin(rotation_angle)],[np.sin(rotation_angle), np.cos(rotation_angle)]])
+    rel_p = np.dot(rotation_matrix,p)
+    return rel_p
+
 # Position Features
 def position(r, r1, p): # inputs must be np.array
-    if r1[1] > r[1]:
+    """if r1[1] > r[1]:
         rotation_angle = -np.arccos(np.dot(np.array([1,0]),r1-r)/(1 * np.linalg.norm(r1-r)))
     else:
         rotation_angle = np.arccos(np.dot(np.array([1,0]),r1-r)/(1 * np.linalg.norm(r1-r)))
-
-    #print("rotation_angle: ", rotation_angle)
     rotation_matrix = np.array([[np.cos(rotation_angle), -np.sin(rotation_angle)],[np.sin(rotation_angle), np.cos(rotation_angle)]])
-    #print("rotation_matrix: \n", rotation_matrix)
-    rel_p = np.dot(rotation_matrix,p-r)#np.array([(p-r)[0]*np.cos(rotation_angle)-(p-r)[1]*np.sin(rotation_angle), (p-r)[0]*np.sin(rotation_angle)+(p-r)[1]*np.cos(rotation_angle)])
-    #print("rel_p: ", rel_p)
-    #print("dist: ", distance(r,r1,p))
+    rel_p = np.dot(rotation_matrix,p-r)"""
+    rel_p = realtive_features(p-r, r1-r)
     rho = np.linalg.norm(p-r)
     if np.array_equal(p-r, np.array([0,0])):
         theta = 0
@@ -32,40 +37,38 @@ def position(r, r1, p): # inputs must be np.array
     return rel_p, rho, theta
 
 def curvature(p, p1, p2):   # p_t, p_t-1 and p_t-2
-    """ return the curvature.
-        Negative if p2 if on the right of p
-    """
-    if p[1] > p1[1]:
+    """if p[1] > p1[1]:
         rotation_angle = -np.arccos(np.dot(np.array([1,0]),p-p1)/(1 * np.linalg.norm(p-p1)))
     else:
         rotation_angle = np.arccos(np.dot(np.array([1,0]),p-p1)/(1 * np.linalg.norm(p-p1)))
     rotation_matrix = np.array([[np.cos(rotation_angle), -np.sin(rotation_angle)],[np.sin(rotation_angle), np.cos(rotation_angle)]])
-    rel_p2 = np.dot(rotation_matrix,p2-p1)
+    rel_p2 = np.dot(rotation_matrix,p2-p1)"""
+    rel_p2 = realtive_features(p2-p1, p-p1)
     c = np.arccos(np.dot(p-p1,p2-p1)/(np.linalg.norm(p-p1) * np.linalg.norm(p2-p1)))
     if rel_p2[1] < 0:
         c = -c
     return c
 
-def velocity(vp, vr):
-    actual_module = np.linalg.norm(vp)
-    ref_module = np.linalg.norm(vr)
-    diff_module = np.linalg.norm(vp - vr)
+def velocity_acceleration(p, r):
+    """ if you pass speed it will compute speed features,
+        acceleration otherwise
+    """
+    actual_module = np.linalg.norm(p)
+    ref_module = np.linalg.norm(r)
+    diff_module = np.linalg.norm(p - r)
     diff_of_modules = ref_module - actual_module
 
-    if vr[1] > 0:
-        rotation_angle = -np.arccos(np.dot(np.array([1,0]),vr)/(1 * np.linalg.norm(vr)))
+    """if r[1] > 0:
+        rotation_angle = -np.arccos(np.dot(np.array([1,0]),r)/(1 * np.linalg.norm(r)))
     else:
-        rotation_angle = np.arccos(np.dot(np.array([1,0]),vr)/(1 * np.linalg.norm(vr)))
+        rotation_angle = np.arccos(np.dot(np.array([1,0]),r)/(1 * np.linalg.norm(r)))
     rotation_matrix = np.array([[np.cos(rotation_angle), -np.sin(rotation_angle)],[np.sin(rotation_angle), np.cos(rotation_angle)]])
-    rel_vp = np.dot(rotation_matrix,vp)
-    speed_angle = np.arccos( np.dot(vr,vp) / (np.linalg.norm(vp) * np.linalg.norm(vr)))
-    if rel_vp[1] < 0:
-        speed_angle = -speed_angle
-    print('speed_angle: ', speed_angle)
-    return actual_module
-
-def acceleration():
-    return 1
+    rel_p = np.dot(rotation_matrix,p)"""
+    rel_p = realtive_features(p, r)
+    angle = np.arccos( np.dot(r,p) / (np.linalg.norm(p) * np.linalg.norm(r)))
+    if rel_p[1] < 0:
+        angle = -angle
+    return actual_module, ref_module, diff_module, diff_of_modules, angle
 
 """# test state features
 r = np.array([1,1])
@@ -78,11 +81,21 @@ p = np.array([car_df.iloc[0]['x'], car_df.iloc[0]['y']])
 rel_p, rho, theta = position(r, r1, p)
 cur = curvature(r1, r, p)
 
-"""vp = np.array([car_df.iloc[0]['speed_x'], car_df.iloc[10]['speed_y']])
-vr = np.array([ref_df.iloc[0]['speed_x'], ref_df.iloc[10]['speed_y']])"""
+"""vp = np.array([car_df.iloc[0]['speed_x'], car_df.iloc[0]['speed_y']])
+vr = np.array([ref_df.iloc[0]['speed_x'], ref_df.iloc[0]['speed_y']])"""
 vr = np.array([1,1])
 vp = np.array([0.5,0.5])
-velocity(vp, vr)
+r = ref_df.iloc[10]
+r1 = ref_df.iloc[9]
+p = car_df.iloc[10]
+p1 = car_df.iloc[9]
+tp = p['curLapTime'] - p1['curLapTime'] # elapsed time between time t ant t-1
+tr = r['curLapTime'] - r1['curLapTime'] # elapsed time between time t ant t-1
+ap = np.array([(p['speed_x'] - p1['speed_x']) / tp, (p['speed_y'] - p1['speed_y']) / tp])
+ar = np.array([(r['speed_x'] - r1['speed_x']) / tr, (r['speed_y'] - r1['speed_y']) / tr])
+velocity_acceleration(vp, vr)
+velocity_acceleration(ap, ar)
+
 
 #plt.arrow(0,0, vr[0], vr[1], head_width=0.01, head_length=0.1, fc='r', ec='r')
 #plt.arrow(0,0, vp[0], vp[1], head_width=0.01, head_length=0.1, fc='g', ec='g')
