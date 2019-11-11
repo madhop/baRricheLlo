bc6834ff825d7a9461918c6b5f5178aa16f3c5e8
Umberto <umbertofazio92@gmail.com>
2019-11-11 15:41:25 +0100

    Drop unnecessary rows

--- preprocess_raw_torcs.py
+++ preprocess_raw_torcs.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 raw_df = pd.read_csv('raw_torcs_data/Test.csv', index_col = False)
-first_rows = raw_df.index[raw_df['time'] == 'time'].tolist()
-first_rows = first_rows + [i-1 for i in first_rows]
-print("first_row: ", first_rows)
-print("Prima: ", raw_df.iloc[first_rows])
-raw_df = raw_df.drop(first_rows)
-print("Dopo: ", raw_df.iloc[first_rows])
+
+# Find rows with header instead of data and drop them
+# Also drop the row before the "header" row because it is just to understand that the lap is finished
+header_rows = raw_df.index[raw_df['time'] == 'time'].tolist()   # rows with headers
+header_rows = header_rows + [i-1 for i in header_rows]          # row before headers
+raw_df = raw_df.drop(header_rows)
