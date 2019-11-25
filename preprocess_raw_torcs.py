53359b8531198c6f347a627eaf1f1530c9c53961
Umberto <umbertofazio92@gmail.com>
2019-11-25 18:09:48 +0100

    Downsampling actual trajectory

--- preprocess_raw_torcs.py
+++ preprocess_raw_torcs.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import compute_state_features as sf
 
 """
     Given raw data 100 Hz
@@ -12,7 +13,7 @@ import matplotlib.pyplot as plt
 track_length = 5780
 
 ## look for all possible CSV files and append all in the same DataFrame
-raw_df  =pd.DataFrame()
+raw_df = pd.DataFrame()
 for d in range(1,32):
     for m in range(1,13):
         for y in range(2):
@@ -32,7 +33,7 @@ raw_df['is_partial'] = 0
 
 ## Drop rows with header instead of data and drop them
 header_rows = raw_df.index[raw_df['curLapTime'] == 'time'].tolist()   # rows with headers
-print("Header_rows: ", header_rows)
+#print("Header_rows: ", header_rows)
 raw_df.drop(header_rows, inplace=True)
 raw_df = raw_df.astype(float)
 raw_df.index = np.arange(raw_df.shape[0])   # reset indexes
@@ -48,11 +49,12 @@ def add_row_at_top(df):
     df = df.sort_index()  # sorting by index
     return df
 
-lap_beginnings = ((raw_df['curLapTime'] - add_row_at_top(raw_df)['curLapTime'] < 0))
+lap_beginnings = ((raw_df['curLapTime'] - add_row_at_top(raw_df)['curLapTime'] < 0))    ## questo è un bel numero
 lap_beginnings = [1] + raw_df.index[lap_beginnings].tolist() + [raw_df.shape[0]]    ## the last is fake, just to know where the last lap finishes
 raw_df.drop(raw_df.tail(1).index,inplace=True)  ## drop last raw of zeros that is now useless
-print("lap_beginnings: ", lap_beginnings)
+#print("lap_beginnings: ", lap_beginnings)
 
+## for each row assign lap number, if it is reference trajectory and if it is partial lap
 bestTime = np.inf
 for i in range(len(lap_beginnings)-1):
     raw_df.loc[lap_beginnings[i]:lap_beginnings[i+1]-1, 'NLap'] = i+1
@@ -64,7 +66,7 @@ for i in range(len(lap_beginnings)-1):
     if raw_df.loc[lap_beginnings[i]:lap_beginnings[i+1]-1, 'Dist'].iloc[-1] < track_length:
         raw_df.loc[lap_beginnings[i]:lap_beginnings[i+1]-1, 'is_partial'] = 1
     ## check if best lap and set it as reference trajectory
-    print(i, 'time: ', raw_df.loc[lap_beginnings[i]:lap_beginnings[i+1]-1, 'curLapTime'].iloc[-1])
+    #print(i, 'time: ', raw_df.loc[lap_beginnings[i]:lap_beginnings[i+1]-1, 'curLapTime'].iloc[-1])
     if raw_df.loc[lap_beginnings[i]:lap_beginnings[i+1]-1, 'curLapTime'].iloc[-1] < bestTime and raw_df.loc[lap_beginnings[i]:lap_beginnings[i+1]-1, 'is_partial'].iloc[0] != 1:
         bestTime = raw_df.loc[lap_beginnings[i]:lap_beginnings[i+1]-1, 'curLapTime'].iloc[-1]
         raw_df['isReference'] = 0
@@ -82,10 +84,29 @@ raw_df['dist_to_middle'] = 2*raw_df['dist_to_middle']/raw_df['trk_width']
 raw_df.drop(columns = 'trk_width', inplace=True)
 
 ## save reference trajectory in 100 Hz
-ref_df = raw_df[raw_df['isReference'] == 1].copy()
+#ref_df = raw_df[raw_df['isReference'] == 1].copy()
+ref_df = pd.DataFrame()
+for index, row in raw_df[raw_df['isReference'] == 1].iterrows():
+    ref_df.loc[index, 'x'] = row['x']
+    ref_df.loc[index, 'y'] = row['y']
+    ## compute alpha step
+    if index == raw_df[raw_df['isReference'] == 1].index[0] or index == raw_df[raw_df['isReference'] == 1].index[-1]:   ## first and last rows
+        alpha_step = 0
+    else:
+        r = np.array([raw_df.loc[index-1, 'x'], raw_df.loc[index-1, 'y']])
+        r1 = np.array([row['x'], row['y']])
+        r2 = np.array([raw_df.loc[index+1, 'x'], raw_df.loc[index+1, 'y']])
+        r2 = r2 - r1
+        r1 = r1 - r
+        alpha_step = sf.compute_alpha(r, r1, r2)
+    ref_df.loc[index, 'alpha_step'] = alpha_step
 
+## Export reference trajectory as CSV file
+ref_df.to_csv(path_or_buf = "trajectory/test_ref_traj.csv", index = False)
 
-
-
+## Downsampling actual trajectory
+raw_df.index = np.arange(raw_df.shape[0])   # reset indexes
+to_drop = raw_df.index[ raw_df.index % 10 != 0].tolist()   # rows with headers
+raw_df.drop(to_drop, inplace=True)
 ## Now data are raw no more. Save to csv
 #raw_df.to_csv(path_or_buf = "trajectory/forza_ow1_steering_w.csv", index = False)
