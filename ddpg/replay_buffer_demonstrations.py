9fc31f15ba63fa61639014079c34470ad2beb039
Umberto <umbertofazio92@gmail.com>
2020-02-06 12:04:13 +0100

    fixed deletion of old transitions

--- ddpg/replay_buffer_demonstrations.py
+++ ddpg/replay_buffer_demonstrations.py
@@ -20,16 +20,19 @@ class PrioritizedReplayBufferDemonstrations(PrioritizedReplayBuffer):
         """
         self.d_size = len(demonstrations)
         super(PrioritizedReplayBufferDemonstrations, self).__init__(self.d_size + buffer_size, alpha)
-        print('Initialize PRBD')
         for t in demonstrations:
             scaled_a = scale_action(action_space, t[1])
             super().add(t[0], scaled_a, *t[2:])
+        print('PRBD Initialized')
 
 
-    def update_priorities(self, batch_idxes, new_priorities):
+    def update_priorities(self, batch_idxes, new_priorities, td_errors):
+        new_priorities = np.abs(td_errors) + self.prioritized_replay_eps + self.prioritized_replay_eps_D
         # TODO: different update for demonstration and non-demonstration
         super.update_priorities(batch_idxes, new_priorities)
 
     def add(self, obs_t, action, reward, obs_tp1, done):
         super().add(obs_t, action, reward, obs_tp1, done)
-        self._next_idx += self.d_size
+        # When the buffer is full, delete the oldest non-demonstrations transition
+        if self._next_idx <= self.d_size:
+            self._next_idx = self.d_size
