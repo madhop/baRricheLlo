0b8798a79e7294510bf0a2b816eaa35583505f6c
Umberto <umbertofazio92@gmail.com>
2020-03-06 19:53:36 +0100

    Controller

---
+++ driver_controller.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 19:48:48 2020
+
+@author: umberto
+Driver controller
+"""
+
+class controller():
+    def __init__(self):
+        # Init
+        self.alpha1 = None
+        self.k1 = None
+        self.beta1 = None
+        self.k2 = None
+        self.gamma1 = None
+        self.gamma2 = None
