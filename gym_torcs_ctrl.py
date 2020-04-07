c23e458a2fbcd17b25ce92921b70580d4c70f945
Umberto <umbertofazio92@gmail.com>
2020-04-07 15:23:44 +0200

    high variance

--- gym_torcs_ctrl.py
+++ gym_torcs_ctrl.py
@@ -161,7 +161,7 @@ class TorcsEnv(gym.Env):
         else:
             # Reward computation
             # Compute the current state
-            current_state = self.observation_to_state(self.observation, self.p1_observation, self.p2_observation,
+            """current_state = self.observation_to_state(self.observation, self.p1_observation, self.p2_observation,
                                                       self.prev_u)
             current_state_df = np.concatenate([current_state.reshape(1, -1), np.zeros((1, 1))], axis=1)
             # Compute the next state
@@ -169,12 +169,12 @@ class TorcsEnv(gym.Env):
             next_state_df = np.concatenate([next_state.reshape(1, -1), np.ones((1, 1)) * obs['trackPos']], axis=1)
             data = pd.DataFrame(data=np.concatenate([current_state_df, next_state_df], axis=0),
                                 columns=self.state_cols + ['trackPos'])
-            reward = self.reward_function(data)
+            reward = self.reward_function(data)"""
             #reward = 0
             # gym-torcs reward
-            """sp = np.array(obs['speed_x'])
+            sp = np.array(obs['speed_x'])
             progress = sp*np.cos(obs['angle']) - np.abs(sp*np.sin(obs['angle'])) - sp * np.abs(obs['trackPos'])
-            reward = progress"""
+            reward = progress
 
 
         # Save u as previous action for the next step
